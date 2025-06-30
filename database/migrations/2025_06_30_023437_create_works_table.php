a133934b0fc7fc4db03e5f6cadd3ff5bac5878c9
muchomucho-tounyu <ckcgbisbm@icloud.com>
2025-06-30 03:03:09 +0000

    first commit

---
+++ database/migrations/2025_06_30_023437_create_works_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->year('release_year');
+            $table->text('overview')->nullable();
+            $table->float('vote_average', 2, 1)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('works');
+    }
+};
---
+++ database/migrations/2025_06_30_023901_create_reviews_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('work_id')->nullable;
+            $table->unsignedBigInteger('person_id')->nullable;
+            $table->unsignedBigInteger('song_id')->nullable;
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('work_id')->references('id')->on('works');
+            $table->foreign('person_id')->references('id')->on('people');
+            $table->foreign('song_id')->references('id')->on('songs');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
