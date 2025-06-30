2afd70c2c0823a510518dea2189edee5eb5d3d52
muchomucho-tounyu <ckcgbisbm@icloud.com>
2025-06-30 10:47:20 +0000

    CreateViewMade

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(Post $post)
+    {
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+    }
+
+
+
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['posts' => $post]);
+    }
+
+    public function create(Post $post)
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+
+    //
+}
